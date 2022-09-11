2d15f9df2e0db070ee09726b13b9db1a7a165499
Wellington Defassio <well.defassio@gmail.com>
2022-09-11 12:52:16 -0300

    micro serviço floricultura - criação de logs com peppertreil

--- src/main/java/com/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/com/microservice/fornecedor/controller/InfoController.java
@@ -2,6 +2,8 @@ package com.microservice.fornecedor.controller;
 
 import com.microservice.fornecedor.model.InfoFornecedor;
 import com.microservice.fornecedor.service.InfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("info")
 public class InfoController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
     private InfoService infoService;
 
     public InfoController(InfoService infoService) {
@@ -20,6 +23,7 @@ public class InfoController {
     @GetMapping("{estado}")
     public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
 
+        LOG.info("recebido pedido de informações do fornecedor de {}", estado);
 
         return infoService.getInfoPorEstado(estado);
 
--- src/main/java/com/microservice/fornecedor/controller/PedidoController.java
+++ src/main/java/com/microservice/fornecedor/controller/PedidoController.java
@@ -3,6 +3,8 @@ package com.microservice.fornecedor.controller;
 import com.microservice.fornecedor.dto.ItemDoPedidoDTO;
 import com.microservice.fornecedor.model.Pedido;
 import com.microservice.fornecedor.service.PedidoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +15,15 @@ public class PedidoController {
 
     private PedidoService pedidoService;
 
+    private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+
     public PedidoController(PedidoService pedidoService) {
         this.pedidoService = pedidoService;
     }
 
     @PostMapping
     public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+        LOG.info("pedido recebido");
         return pedidoService.realizaPedido(produtos);
     }
 
